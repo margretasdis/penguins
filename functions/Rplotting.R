5b1a4963b885041669bf1e9e9d2150be1b388c66
margretasdis <117742503+margretasdis@users.noreply.github.com>
2022-12-07 18:52:10 +0000

    Add files via upload

---
+++ functions/Rplotting.R
@@ -0,0 +1,27 @@
+
+plot_beak_plot <- function(penguins_culmen) {
+  penguins_culmen %>%
+  ggplot(aes(x = body_mass_g, y = culmen_length_mm, colour = species, shape = species), alpha = 0.05) +
+  geom_point(alpha = 0.7) +
+  geom_smooth(method = "lm", se = TRUE) +
+  theme_bw() +
+  labs(x = "\nBody mass (g)", y = "Beak length (mm)\n", colour = "Penguin species", shape = "Penguin species", title = "Relationship between penguin bodymass and beak length", subtitle = "Bill length and body mass for Adelie, Chinstrap and Gentoo Penguins at Palmer Station LTER") +
+  scale_color_manual(values = c("dodgerblue","darkorange2","darkorchid4"))
+}
+
+plot_beak_plot(penguins_culmen)
+
+#Plot the data with our model lines included to aid with the interpretation
+
+r Simple plot for aiding the interpretation of the statistical test}
+
+plot_beak_boxplot <- ggplot(penguins_culmen, aes(x = body_mass_g, y = culmen_length_mm, colour = species, shape = species), alpha = 0.05) +
+  geom_point(alpha = 0.7) +
+  geom_smooth(method = "lm", se = TRUE) +
+  theme_bw() +
+  labs(x = "\nBody mass (g)", y = "Beak length (mm)\n", colour = "Penguin species", shape = "Penguin species") +
+  scale_color_manual(values = c("darkorange","purple","cyan4"))
+
+plot_beak_boxplot
+
+#colour intervals, remove latin names?
